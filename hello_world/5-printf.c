8a76946acb4b7ca7e55684d7c90992ddf6d02e45
JCRoooD <juancarlos-99@live.com>
2023-06-17 19:17:44 +0000

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Printf
  *
- * Return = Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n";
 return (0);
 }
