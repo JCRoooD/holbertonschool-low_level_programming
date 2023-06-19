c7d3229b20e11400aaded6545f718cc63c107a0b
JCRoooD <juancarlos-99@live.com>
2023-06-19 22:33:53 +0000

    alphaBET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result: Always 0
+ */
+int main(void)
+{
+	char AB;
+	for (AB = 'a' ; AB <= 'z' ; AB++)
+		putchar (AB);
+	for (AB = 'A' ; AB <= 'Z' ; AB++)
+		putchar (AB);
+
+	putchar ('\n')
+		return (0);
+}
