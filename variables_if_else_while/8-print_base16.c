e922f29a1faa0e9233c9ffd2bc41b77bf28fbe12
JCRoooD <juancarlos-99@live.com>
2023-06-20 14:25:54 +0000

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	char x;
+
+	for (AB = '0' ; AB <= '9' ; AB++)
+		putchar (x);
+	for (AB = 'a' ; AB <= 'f' ; AB++)
+		putchar (x);
+
+	putchar('\n')
+		return (0);
+}
