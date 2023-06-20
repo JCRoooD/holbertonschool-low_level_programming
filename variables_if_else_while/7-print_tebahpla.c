bc14a3b329ba125fb6229aa593fef09ac39fd129
JCRoooD <juancarlos-99@live.com>
2023-06-20 13:48:37 +0000

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char AB;
+
+	for (AB = 'a' ; AB <= 'z' ; AB--)
+		putchar (AB);
+
+putchar ('\n');
+	return (0);
+}
