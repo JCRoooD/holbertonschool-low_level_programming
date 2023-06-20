d740910d9e465d5b284f3579a96a8c47523f85e5
JCRoooD <juancarlos-99@live.com>
2023-06-20 02:16:07 +0000

    Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - entry point
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	int x;
-	for(x = 0; x < 10; x++)
-		putchar (x);
 
-	putchar ('\n');
-
-		return (0);
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
 }
