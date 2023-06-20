3500cdcae0d2e50dc29aa28e53f0808ec848e520
JCRoooD <juancarlos-99@live.com>
2023-06-20 20:35:42 +0000

    print alphabet lowercase with prototype function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - entry point play the alphabet game
+ * print_alphabet - entry point play the alphabet game
  * Return: Always 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char x;
 
