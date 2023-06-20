c4c95ba06ce47567267bd15698d0e9f688e10278
JCRoooD <juancarlos-99@live.com>
2023-06-20 15:22:34 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 
 	for (x = 48; x < 58; x++)
 	{
-	putchar (x);
-	if (x < 57)
-	printf("combination for all single digits", x);
-	}
-
-	putchar (44);
+		putchar (x);
+		if (x < 57)
+		{
+		putchar (44);
 		putchar(32);
+		}
+	}
 		putchar('\n');
 		return (0);
 }
