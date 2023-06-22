d389511d7165bdd7c69036c0a6084654dc436e38
JCRoooD <juancarlos-99@live.com>
2023-06-22 14:51:12 +0000

    printing and returning value of last number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *print_last_digit -last digit value
+ *Return: value of last digit
+ *@n:last digit value will be printed
+ */
+int print_last_digit(int n)
+{
+
+	n = n % 10;
+	_putchar(n);
+
+	return (abs (n));
+}
