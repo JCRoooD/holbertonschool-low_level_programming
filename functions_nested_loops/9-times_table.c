6bbd30091145902f21a6545c160e36de4dece7b6
JCRoooD <juancarlos-99@live.com>
2023-06-23 17:34:18 +0000

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,13 +16,12 @@ void times_table(void)
 				putchar('0');
 				continue;
 			}
-		
 			if ((c * r) >= 10)
 			{
-			putchar(44);
-			putchar(32);
-			putchar((r * c) / 10 + '0');
-			putchar((r * c) % 10 + '0');
+				putchar(44);
+				putchar(32);
+				putchar((r * c) / 10 + '0');
+				putchar((r * c) % 10 + '0');
 			}
 			else
 			{
