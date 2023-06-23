3a903ed70e40edac9d543be0bd302d9e07bc42b6
JCRoooD <juancarlos-99@live.com>
2023-06-23 03:30:32 +0000

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add -  print result of the sum
+ * Return: sum of the two integers
+ * @q: first integer
+ * @p: second integer
+ */
+
+int add(int q, int p)
+{
+	int x;
+
+	x = q + p;
+
+	return (x);
+}
