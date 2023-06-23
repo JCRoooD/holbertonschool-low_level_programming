4cb583bf1434645ef0b1690041619401dab5af77
JCRoooD <juancarlos-99@live.com>
2023-06-23 03:36:30 +0000

    a+b

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * add -  print result of the sum
+ * add - print the sum of the integers
  * Return: sum of the two integers
  * @q: first integer
  * @p: second integer
