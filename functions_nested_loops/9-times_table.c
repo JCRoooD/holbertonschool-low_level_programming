10d2eb2b8d446554cfd66473eb8d61f63b282190
JCRoooD <juancarlos-99@live.com>
2023-06-23 14:35:10 +0000

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 /**
  * times_table - nine times table
- * @n: will print the 9 times table
+ * @c: columns
+ * @r: rows
  */
 void times_table(void);
 {
-	int n, y;
+	int c, r;
 
-
-
-
-
-
-
+	for(r >= 0; r <= 9; r++)
+	{
+		for(c >= 0; c <= 9; c++)
+		{
+			putchar(r*c);
+			putchar(32);
+			putchar(44);
+		}
+	}
+	putchar('\n');
 }
