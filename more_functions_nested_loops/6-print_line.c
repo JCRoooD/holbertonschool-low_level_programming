f7f6d72d9d207201a3f7232e8173bb9790c23156
JCRoooD <juancarlos-99@live.com>
2023-06-24 03:24:02 +0000

    drawing a line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,18 @@
  */
 void print_line(int n)
 {
-
-	for (n < 0; n <= 9; n++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar(n);
-		}
+		_putchar('\n');
+	}
+	else if
+
+		int g;
+
+	for (g = 1; g <= n; g++)
+	{
+		_putchar('_');
 	}
 	_putchar('\n');
+}
 }
