0cae3d47a5b1efa29002ba231f9a8b2a6f88f683
JCRoooD <juancarlos-99@live.com>
2023-06-24 03:44:45 +0000

    FizzBizz

---
+++ more_functions_nested_loops/9-fizz-buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Fizz-Bizz
+ * Return: Count it up
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 1; z <= 100; z++)
+	{
+		if (z % 3 == 0 && z % 5 == 0)
+			printf("FizzBuzz ");
+		else if (z % 3 == 0)
+			printf("Fizz ");
+		else if (z % 5 == 0)
+
+			if (z != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		else
+			printf("%d ", z);
+	}
+	printf("\n");
+	return (0);
+}
