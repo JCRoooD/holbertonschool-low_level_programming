323106a5b36baef5b027db3866909588c9959659
JCRoooD <juancarlos-99@live.com>
2023-06-24 01:50:51 +0000

    10 times zero to fourteen

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - zero to fourteen real quick
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 14; x++)
+	{
+		for (y >= 0; y <= 10; y++)
+		{
+			_putchar(y);
+		}
+		putchar(x);
+	}
+	_putchar('\n');
+}
