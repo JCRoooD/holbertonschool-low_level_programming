ebb16fcc05a6c36ef741e2c9451c6feae9970b08
JCRoooD <juancarlos-99@live.com>
2023-06-26 18:49:47 +0000

    string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - get the length of a string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	;
+	{
+	return (x);
+	}
+}
