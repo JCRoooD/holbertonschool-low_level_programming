6d216e106be34995c0ec9290a053feb668fe22be
JCRoooD <juancarlos-99@live.com>
2023-06-26 17:29:35 +0000

    swapping integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _swap_int - swapping values
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 3;
+	*b = 6;
+
+	a = 6;
+	b = 3;
+}
