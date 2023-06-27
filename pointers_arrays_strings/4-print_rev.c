5f50ef5c407c8ac93f2574b8828586c7e309931c
JCRoooD <juancarlos-99@live.com>
2023-06-27 01:34:27 +0000

    reverse gear

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - printing reverse string
+ * @s: character
+ * @length: length of string
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*(s + length))
+		length++;
+
+	length = length -1;
+
+	while (length >= 0)
+	{
+		_putchar(*(s + length));
+		length--;
+	}
+	_putchar('\n');
+}
