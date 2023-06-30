135a8077904a1ffd73f7aa748818342c7b57dc0f
JCRoooD <juancarlos-99@live.com>
2023-06-30 03:47:07 +0000

    Write a function that compares two strings.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcmp - function that compares two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	/*comparing both strings*/
+	while (*s1 == *s2)
+	{
+		/*checks if the current character being pointed to by s1 is the null terminator ('\0').*/
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+			s1++;
+			s2++;
+	}
+	return (*s1 - *s2);
+}
