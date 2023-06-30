762fa1c7a0f905017751025209eff3067d907494
JCRoooD <juancarlos-99@live.com>
2023-06-30 19:15:21 +0000

    Write a function that capitalizes all words of a string.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *cap_string(char *x)
+{
+
+	int counter = 0;
+	int capi = 32;
+	char sepa[13] = {',', ';', '.', '!', '?', '"',
+	'(', ')', '{', '}', ' ', '\t', '\n'};
+	int val = 0;
+
+	while (x[counter] != '\0')
+	{
+		if (x[counter] >= 'a' && x[counter] <= 'z' && counter == 0)
+		{
+			x[counter] = x[counter] - capi;
+		}
+		
+		while (val < 13)
+		{
+			if (x[counter] == sepa[val])
+			{
+				if (x[counter + 1] >= 'a' && x[counter] <= 'z')
+				{
+					x[counter + 1] = x[counter + 1] - capi;
+				}
+			}
+			val++;
+		}
+		counter++;
+	}
+	return (x);
+}
