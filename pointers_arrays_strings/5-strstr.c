f01649784cff81144947ace5e51fd40ecf79bc65
JCRoooD <juancarlos-99@live.com>
2023-07-03 13:03:39 +0000

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,23 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 /**
  **_strstr - substring to be located
- *@haystack: pointer to v
- *@needle: pointer to f
+ *@haystack: pointer with varible v
+ *@needle: pointer with variable f
  *Return: 0
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int k = 0;
 	int v = 0;
-	int f = 0;
 
-	while (haystack[v] != '\0')
+	while (haystack[k] != '\0')
 	{
-		if (haystack[v] == needle[f])
+		while (needle[v] != '\0')
 		{
-		f++;
-		v++;
+			if (haystack[k + v] != needle[v])
+				break;
+			v++;
 		}
-		break;
+
+		if (needle[v] != '\0')
+			return (&haystack[k]);
+		k++;
 	}
 	return (0);
 }
