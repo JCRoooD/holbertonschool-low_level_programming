44c43a987d032dbae80d7911513a27ff22b542f8
JCRoooD <juancarlos-99@live.com>
2023-08-02 19:29:20 +0000

    adding a node at the end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -34,9 +34,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		{
 			temp = temp->next;
 		}
-	
-	temp->next = n_node;
-	n_node->prev = temp;
+
+		temp->next = n_node;
+		n_node->prev = temp;
 	}
 	return (n_node);
 }
