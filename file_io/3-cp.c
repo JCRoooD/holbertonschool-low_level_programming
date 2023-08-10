0e5756144aa255c12641795469d117d9fa4d5eec
JCRoooD <juancarlos-99@live.com>
2023-08-10 01:43:17 +0000

    udated code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - main function
  * @argc: count of arguments
@@ -9,7 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fd_f, fd_t, c1, c2;
+	int fd_from, fd_to, count_r, count_w;
 	char *file_f = argv[1];
 	char *file_t = argv[2];
 	char *buff[1024];
@@ -17,31 +16,31 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		error_and_exit("cp file_from file_to", NULL, 97);
 
-	fd_f = open(file_f, O_RDONLY);
-	if (fd_f == -1 || file_f == NULL)
+	fd_from = open(file_f, O_RDONLY);
+	if (fd_from == -1 || file_f == NULL)
 		error_and_exit("Can't read from file", file_f, 98);
 
-	fd_t = open(file_t, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_t == -1 || file_t == NULL)
+	fd_to = open(file_t, O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd_to == -1 || file_t == NULL)
 		error_and_exit("Can't write to", file_t, 99);
 
 	while (1)
 	{
-		c1 = read(fd_f, buff, 1024);
-		if (c1 == 0)
+		count_r = read(fd_from, buff, 1024);
+		if (count_r == 0)
 			break;
-		if (c1 == -1)
+		if (count_r == -1)
 			error_and_exit("Can't read from file", file_f, 98);
 
-		c2 = write(fd_t, buff, c1);
-		if (c2 == -1)
+		count_w = write(fd_to, buff, count_r);
+		if (count_w == -1)
 			error_and_exit("Can't write to", file_t, 99);
 	}
 
-	if (close(fd_f) == -1)
+	if (close(fd_from) == -1)
 		error_and_exit("Can't close fd", file_f, 100);
 
-	if (close(fd_t) == -1)
+	if (close(fd_to) == -1)
 		error_and_exit("Can't close fd", file_t, 100);
 
 	return (0);
