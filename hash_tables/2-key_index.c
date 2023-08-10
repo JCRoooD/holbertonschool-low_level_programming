6358454e60374a14998e4a3b830277f28a3cf8d0
JCRoooD <juancarlos-99@live.com>
2023-08-10 16:27:11 -0400

    update code

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,10 +5,10 @@
 *@size: size of the array in hash table
 *Return: the index at which the key/value pair should be stored in the array of the hash table
 */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     if (key == NULL || size == 0)
-        return (NULL);
+        return (0);
 
     return (hash_djb2(key) % size);
 }
