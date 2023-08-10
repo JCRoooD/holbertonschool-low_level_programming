587dca83327fac4fe303ee13b3e559548b727ce0
JCRoooD <juancarlos-99@live.com>
2023-08-10 16:32:46 -0400

    updated for the 5th time

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,7 +3,7 @@
  *key_index - function that gives index of a key
  *@key: is the key
  *@size: size of the array in hash table
- *Return: index at which the key/value pair stored in the array of the hash table
+ *Return: index of key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
